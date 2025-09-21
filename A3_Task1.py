7bb4348d77256adba434a1b8d48e9c49e5dba235
Dr-Hemant <hemantkumarjangid122001@gmail.com>
2025-09-21 19:02:49 +0530

    Assignment Completed

--- A3_Task1.py
+++ A3_Task1.py
@@ -1,1 +1,11 @@
+#Printing Factorial of a Number given by user
+def factorial(n):
+   if (n == 0 or n == 1):
+       return 1
+   else :
+       return n * factorial(n-1)
+
+
+num = int(input("Enter Number: "))
+print(f"Factorial of {num} is {factorial(num)}.")
 
